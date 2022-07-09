b2579d0002922282d7211d73074b04a7db45f481
AnonymousBacon <46059740+AnonymousBacon@users.noreply.github.com>
2022-07-08 17:57:20 -0700

    .

--- Bronze/src/mixingmilk.java
+++ Bronze/src/mixingmilk.java
@@ -8,10 +8,9 @@ public class mixingmilk {
 	static int m1, m2, m3, c1, c2, c3;
 	
 	public static void main(String[]args) throws IOException {
-//		in = new Scanner(new File(".in"));
-		in = new BufferedReader(new FileReader(".in"));
-		out = new PrintWriter(new BufferedWriter(new FileWriter(".out")));
-		
+//		in = new Scanner(System.in);
+		in = new BufferedReader(new FileReader("mixmilk.in"));
+		out = new PrintWriter(new BufferedWriter(new FileWriter("mixmilk.out")));
 		
 		init();
 		solve();
@@ -21,17 +20,61 @@ public class mixingmilk {
 	
 	static void init() throws IOException {
 		StringTokenizer st = new StringTokenizer(in.readLine());
-		m1 = Integer.parseInt(st.nextToken());
 		c1 = Integer.parseInt(st.nextToken());
+		m1 = Integer.parseInt(st.nextToken());
 		st = new StringTokenizer(in.readLine());
-		m2 = Integer.parseInt(st.nextToken());
 		c2 = Integer.parseInt(st.nextToken());
+		m2 = Integer.parseInt(st.nextToken());
 		st = new StringTokenizer(in.readLine());
-		m3 = Integer.parseInt(st.nextToken());
 		c3 = Integer.parseInt(st.nextToken());
+		m3 = Integer.parseInt(st.nextToken());
 	}
 	
 	static void solve() {
-		
+		for (int i = 1; i <= 33; i++) {
+			int temp = 0;
+			if (m2+m1 > c2) {
+				temp = m1;
+				m1 = (m2+m1)-c2;
+				m2 = c2;
+			}
+			else {
+				m2 += m1;
+				m1 = 0;
+			}
+			
+			if (m2+m3 > c3) {
+				temp = m2;
+				m2 = (m2+m3)-c3;
+				m3 = c3;
+			}
+			else {
+				m3 += m2;
+				m2 = 0;
+			}
+			
+			if (m3+m1 > c1) {
+				temp = m3;
+				m3 = (m1+m3)-c1;
+				m1 = c1;
+			}
+			else {
+				m1 += m3;
+				m3 = 0;
+			}
+		}
+		int temp = 0;
+		if (m2+m1 > c2) {
+			temp = m1;
+			m1 = (m2+m1)-c2;
+			m2 = c2;
+		}
+		else {
+			m2 += m1;
+			m1 = 0;
+		}
+		out.println(m1);
+		out.println(m2);
+		out.println(m3);
 	}
 }
--- Bronze/src/template.java
+++ Bronze/src/template.java
@@ -2,13 +2,13 @@ import java.io.*;
 import java.util.*;
 
 public class template {
-//	static Scanner in;
-	static BufferedReader in;
+	static Scanner in;
+//	static BufferedReader in;
 	static PrintWriter out;
 	
 	public static void main(String[]args) throws IOException {
-//		in = new Scanner(new File(".in"));
-		in = new BufferedReader(new FileReader(".in"));
+		in = new Scanner(System.in);
+//		in = new BufferedReader(new FileReader(".in"));
 		out = new PrintWriter(new BufferedWriter(new FileWriter(".out")));
 		
 		
@@ -19,7 +19,7 @@ public class template {
 	}
 	
 	static void init() throws IOException {
-		StringTokenizer st = new StringTokenizer(in.readLine());
+		StringTokenizer st = new StringTokenizer(in. nextLine());
 	}
 	
 	static void solve() {
